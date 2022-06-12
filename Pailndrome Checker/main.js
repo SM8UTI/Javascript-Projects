6e3174e208fd898cde7b327bd84cf4bf6a8a2007
Smruti Ranjan Nayak <56423859+SM8UTI@users.noreply.github.com>
2022-06-12 09:33:43 +0530

    pailndrome code updates

---
+++ Pailndrome Checker/main.js
@@ -0,0 +1,24 @@
+
+
+const txtInput = document.querySelector(".inputs input"),
+checkBtn = document.querySelector(".inputs button"),
+infoTxt = document.querySelector(".info-txt");
+let filterInput;
+
+checkBtn.addEventListener("click", () => {
+    let reverseInput = filterInput.split("").reverse().join("");
+    infoTxt.style.display = "block";
+    if(filterInput != reverseInput) {
+        return infoTxt.innerHTML = `No, <span class="text-blue-700">'${txtInput.value}'</span> isn't a palindrome!`;
+    }
+    infoTxt.innerHTML = `Yes, <span class="text-blue-700">'${txtInput.value}'</span> is a palindrome!`;
+});
+
+txtInput.addEventListener("keyup", () => {
+    filterInput = txtInput.value.toLowerCase().replace(/[^A-Z0-9]/ig, "");
+    if(filterInput) {
+        return checkBtn.classList.add("active");
+    }
+    infoTxt.style.display = "none";
+    checkBtn.classList.remove("active");
+});
