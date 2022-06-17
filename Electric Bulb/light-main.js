7730335d244f9b73b534d0869b39ab0b58b6be61
Smruti Ranjan Nayak <56423859+SM8UTI@users.noreply.github.com>
2022-06-17 23:05:34 +0530

    Add files via upload

--- Electric Bulb/light-main.js
+++ Electric Bulb/light-main.js
@@ -5,11 +5,11 @@ btn.addEventListener("click",function(){
     if(btn.textContent == "ON"){
         btn.textContent = "OFF";
         // imgBtn.src="image/Images/ON.png";
-        imgBtn.src="Images/extra.png";
+        imgBtn.src="https://sm8uti.github.io/Javascript-Projects/Electric%20Bulb/Images/extra.png";
     }
     else{
         btn.textContent = "ON";
-        imgBtn.src="Images/Off.png";
+        imgBtn.src="https://sm8uti.github.io/Javascript-Projects/Electric%20Bulb/Images/Off.png";
     }
 });
 
